db25e95fefdba3509578cd016b22aaf87ee02d40
Jaden O'Brien <jadenobrien4@gmail.com>
2022-12-15 00:41:28 -0600

    Setup file IO for day5 project

--- DayFive/Program.cs
+++ DayFive/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿namespace DayFive
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            string[] input = System.IO.File.ReadAllLines("test.txt");
+
+            foreach(string line in input)
+            {
+                System.Console.WriteLine(line);
+            }
+
+        }
+
+    }
+
+}
